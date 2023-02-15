b2bafc1fd8e86f80a887d8b4007395700c40cd6b
TÉCNICO EM INFORMÁTICA <51127512022.2@senacrjedu.local>
2023-02-15 12:10:55 -0300

    continuacao do blog - database adicionado

--- src/main/java/com/senacniteroi/blogpessoal/model/Postagem.java
+++ src/main/java/com/senacniteroi/blogpessoal/model/Postagem.java
@@ -1,14 +1,14 @@
 package com.senacniteroi.blogpessoal.model;
 
-
 import java.time.LocalDateTime;
 
 import org.hibernate.annotations.UpdateTimestamp;
-import org.springframework.data.annotation.Id;
+
 
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import jakarta.persistence.Table;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
@@ -17,6 +17,38 @@ import jakarta.validation.constraints.Size;
 @Table(name = "tb_postagens")
 public class Postagem {
 	
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getTitulo() {
+		return titulo;
+	}
+
+	public void setTitulo(String titulo) {
+		this.titulo = titulo;
+	}
+
+	public String getTexto() {
+		return texto;
+	}
+
+	public void setTexto(String texto) {
+		this.texto = texto;
+	}
+
+	public LocalDateTime getData() {
+		return data;
+	}
+
+	public void setData(LocalDateTime data) {
+		this.data = data;
+	}
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
